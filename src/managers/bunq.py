77c958f86a9733bf85a2111bc20f1a6baade6b9e
Kees Reusen <k.reusen@gmail.com>
2018-06-11 00:09:47 +0200

    make it possible to read api_context from aws

--- src/managers/bunq.py
+++ src/managers/bunq.py
@@ -7,15 +7,21 @@ class BunqManager():
     def __init__(self):
         self.parameter_service = ParameterService()
 
-        if not self.parameter_service.exists("bunq/api_context"):
-            self.create_api_context()
-    
-    def create_api_context(self):
+        if self.parameter_service.exists("/bunq/api_context"):
+            self.api_context = self._get_api_context_from_aws()
+        else:
+            self.api_context = self._create_api_context()
+        
+    def _get_api_context_from_aws(self):
+        json_string = self.parameter_service.get("/bunq/api_context")
+        return ApiContext.from_json(json_string)
+
+    def _create_api_context(self) -> ApiContext:
         api_key = self.parameter_service.get('/bunq/api_key')
         api_context = ApiContext(
             ApiEnvironmentType.PRODUCTION, 
             api_key,
             'runs-to-gadgetfund'
         )
-        self.parameter_service.store("bunq", api_context.to_json())
-        
+        self.parameter_service.store("/bunq/api_context", api_context.to_json())
+        return api_context
